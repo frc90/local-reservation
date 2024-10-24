b7d9fa6b0036520357b6901b8122414cb688d306
frc90 <39072359+frc90@users.noreply.github.com>
2024-10-24 12:06:28 -0300

    Initial commit -> hello world!!!

---
+++ src/main/java/com/local_reservation/LocalReservationApplication.java
@@ -0,0 +1,13 @@
+package com.local_reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalReservationApplication.class, args);
+	}
+
+}
